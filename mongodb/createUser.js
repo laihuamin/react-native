4f61f22b89b6bb0fbc4cc88d3f2b58ba81d25836
laihuamin <1445509994@qq.com>
2017-12-30 11:52:58 +0800

    批量写入

--- mongodb/createUser.js
+++ mongodb/createUser.js
@@ -3,5 +3,62 @@ require('./connect.js')
 const mongoose = require('mongoose')
 const fs = require('fs')
 
-const userData = JSON.parse(fs.readFileSync('userData.json', 'utf-8'));
+const userData = JSON.parse(fs.readFileSync('./mongodb/userData.json', 'utf-8'));
 
+// 定义骨架
+UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    partment: {
+        type: String,
+        required: true
+    },
+    tel: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    tag: {
+        type: String,
+        required: true
+    },
+    creater: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        required: true
+    }
+})
+
+// 定义模型
+
+const User = mongoose.model('User', UserSchema);
+
+userData.forEach((item) => {
+    const tempItem = new User({
+        name: item.name,
+        password: item.password,
+        partment: item.partment,
+        tel: item.tel,
+        email: item.email,
+        tag: item.tag,
+        creater: item.creater,
+        token: item.token
+    })
+    tempItem.save(function(err) {
+        if(err) {
+            throw err;
+        }
+    })
+});
